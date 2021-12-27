db3cb2f2c4fff2774a1b5076f945969148ae61de
Oleg Grynkevych <oleg252.og@gmail>
2021-12-27 02:39:08 +0200

    Add slider

--- homework-14/js/main.js
+++ homework-14/js/main.js
@@ -6,3 +6,11 @@ window.onload = function () {
     let preloader = document.getElementById('preloader');
     preloader.style.display = 'none';
 }
+
+$(document).ready(function() {
+    $('.review-slider').slick({
+        autoplay: true,
+        nextArrow: '.arrow-right',
+        prevArrow: '.arrow-left'
+    });
+  });
